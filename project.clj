855008ec555da009b21a706c26070b0ff2afc8ff
Michael S. Klishin <michael@novemberain.com>
2012-04-05 19:36:39 +0400

    Clostache 1.3.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure             "1.3.0"]
                  [com.draines/postal              "1.7.1"]
-                 [de.ubercode.clostache/clostache "1.2.0"]
+                 [de.ubercode.clostache/clostache "1.3.0"]
                  [clojurewerkz/support            "0.1.0-beta2"]
                  [clojurewerkz/route-one          "1.0.0-beta1"]]
   :test-selectors {:default        (fn [m] (and (not (:time-consuming m))
