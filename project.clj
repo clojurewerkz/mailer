fd6f4597e523f3c71eee67409b3ca16b890b4833
Michael Klishin <michael@clojurewerkz.org>
2014-01-26 17:26:57 +0400

    Postal 1.11.1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure             "1.5.1"]
-                 [com.draines/postal              "1.10.3"]
+                 [com.draines/postal              "1.11.1"]
                  [de.ubercode.clostache/clostache "1.3.1"]
                  [clojurewerkz/support            "0.20.0"]
                  [clojurewerkz/route-one          "1.0.0"]]
