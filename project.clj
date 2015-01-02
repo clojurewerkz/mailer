022eac5e62c3ce88bbdabb9ab05d161869d1afac
Michael Klishin <michael@clojurewerkz.org>
2015-01-02 13:46:59 +0300

    Drop 1.4 and 1.5 profiles

--- project.clj
+++ project.clj
@@ -12,11 +12,9 @@
                    :all            (constantly true)}
   :source-paths ["src/clojure"]
   :profiles {:dev {:resource-paths ["test/resources"]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
-  :aliases { "all" ["with-profile" "dev:dev,1.4:dev,1.5:dev,1.7:dev,master"] }
+  :aliases { "all" ["with-profile" "dev:dev,1.7:dev,master"] }
   :repositories {"clojure-releases" "http://build.clojure.org/releases"
                  "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
